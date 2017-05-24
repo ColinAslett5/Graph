ed06a5c560e274e7a6c8a2691b14392a490c6c07
Colin Aslett <280929@bsd48.org>
2017-05-23 20:15:40 -0700

    First commit, Vector printing/other error

---
+++ title.cpp
@@ -0,0 +1,69 @@
+//Graph Creator, C++ Colin Aslett, Period 07
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void vertex(vector<char*> &ver);//adding vertex to the graph, if it can
+void print(vector<char*> ver);
+int main(){
+  cout << "Welcome to Graph Creator" << endl;
+  vector<char*> labels;
+  while(true){
+    char input;
+    cout << "Several Options: 1 = Add vertex, 2 = Add edge, 3 = Remove Vertex, 4 = remove Edge, 5 = add double edge, 6 = find path, 7 = print, q = quit" << endl;
+    cin >> input;
+    //Adding vertex
+    if(input == '1'){
+      vertex(labels);
+    }
+    //Adding Edge
+    else if(input == '2'){
+
+    }
+    //Remove Vertex
+    else if(input == '3'){
+      
+    }
+    //Remove Edge
+    else if(input == '4'){
+      
+    }
+    //Add double Edge
+    else if(input == '5'){
+      
+    }
+    //Find Path
+    else if(input == '6'){
+      
+    }
+    //Printing the Adjcancy Matrix
+    else if(input == '7'){
+      print(labels);
+    }
+    //Quitting program
+    else if(input == 'q'){
+      break;
+    }
+  }
+  return 0;
+}
+//adding vertex to graph if not already used
+void vertex(vector<char*> &ver){
+  char label[16];
+  cout << "Enter name for vertex: " << endl;
+  cin >> label;
+  //check to make sure it is already not in the graph, through iteration
+  //adding it to the vector
+  ver.push_back(label);
+}
+//printing out the Adjcancy Matrix
+void print(vector<char*> ver){
+  int size = ver.size();
+  cout << "Size of Vertex: " << size << endl;
+  for(int i = 0;i < size;i++){
+    cout << ver[i] << " , " << endl;
+  }
+  if(size == 0){
+    cout << "There are no items in the Graph!" << endl;
+  }
+}
